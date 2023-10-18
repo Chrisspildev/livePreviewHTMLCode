bd1dafdd4f3e9d9ea679dedb9a94d42d5d9deffe
Chrisspildev <113357978+Chrisspildev@users.noreply.github.com>
2023-10-18 18:29:15 +0300

    change

--- app.js
+++ app.js
@@ -6,3 +6,21 @@ document.addEventListener("DOMContentLoaded", () => {
     $preview.innerHTML = e.target.value;
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const colors = ['navy', "aqua", "teal", "green", "yellow", "maroon",];
+  const $btn = document.querySelector("button");
+  const $select = document.querySelector("select");
+  const body = document.querySelector("body");
+
+  $btn.addEventListener("click", (e) => {
+    const colorIndex = parseInt(colors.length * Math.random());
+    body.style.backgroundColor = colors[colorIndex];
+  })
+
+  $select.addEventListener("input", (e) => {
+    body.classList.remove("navy", "teal", "purple")
+    body.classList.add(e.target.value);
+  })
+
+})
